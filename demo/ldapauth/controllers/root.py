d064af784747026d991d4407f7fce6fc90de2777
Gustavo Narea <me@gustavonarea.net>
2008-09-07 14:23:54 +0200

    Minor improvements to the demo app

--- demo/ldapauth/controllers/root.py
+++ demo/ldapauth/controllers/root.py
@@ -1,9 +1,11 @@
 """Main Controller"""
-from ldapauth.lib.base import BaseController
-from tg import expose, flash
 
-from pylons.controllers.util import abort
+from tg import expose, flash
 from pylons import request
+from paste.httpexceptions import HTTPUnauthorized
+
+from ldapauth.lib.base import BaseController
+
 
 class RootController(BaseController):
     @expose('ldapauth.templates.index')
@@ -13,7 +15,7 @@ class RootController(BaseController):
     @expose('ldapauth.templates.about')
     def about(self):
         if request.environ.get('repoze.who.identity') == None:
-            abort(401)
+            raise HTTPUnauthorized()
         user = request.environ['repoze.who.identity']['repoze.who.userid']
         flash('Your Distinguished Name (DN) is "%s"' % user)
         return dict()
